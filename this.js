bf4b82fc73a0c3d8f3e1d0a8ddc63b9136a16fbb
githubscientist <sathish.datascientist@gmail.com>
2024-09-20 19:59:17 +0530

    doubt topics --this --hof

---
+++ this.js
@@ -0,0 +1,13 @@
+// function prototype
+function Student(id, name) {
+    this.id = id;
+    this.name = name;
+}
+
+// instantiate the Student class
+let student101 = new Student(101, 'John');
+
+let student102 = new Student(102, 'Jane');
+
+console.log(student101);
+console.log(student102);
