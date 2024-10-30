0fb6c628aac56b02b0e18a08dcbbca72bd509e35
githubscientist <sathish.datascientist@gmail.com>
2024-10-30 20:42:59 +0530

    mark favorite jobs --done

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import { useLoaderData, useNavigate, useSearchParams } from "react-router-dom";
+import './Jobs.css';
+import jobServices from "../services/jobServices";
 
 const Jobs = () => {
 
@@ -7,13 +10,25 @@ const Jobs = () => {
     const query = search.get("q");
     const navigate = useNavigate();
 
-    const handleClick = (id) => {
+    const handleCardClick = (id) => {
         navigate(`/jobs/${id}`);
+    }
+
+    const handleFavoriteClick = async (id) => {
+        // update the job with the given id for the isFavorite field
+        try {
+            await jobServices.updateJob(id, { isFavorite: !jobs.find(job => job.id === id).isFavorite });
+
+            // reload the page
+            navigate(`/jobs?q=${query}`);
+        } catch (error) {
+            alert("An error occurred while updating the job");
+        }
     }
 
     return (
         <div
-            className="container mx-8 my-8"
+            className="container mx-8 my-8 mx-auto"
         >
             <h1
                 className="text-2xl font-bold"
@@ -25,7 +40,7 @@ const Jobs = () => {
                         <div
                             key={job.id}
                             className="my-4 p-4 border border-gray-200 flex flex-col gap-2"
-                            onClick={() => handleClick(job.id)}
+                            onClick={() => handleCardClick(job.id)}
                         >
                             <h2
                                 className="text-lg font-bold"
@@ -38,19 +53,35 @@ const Jobs = () => {
                             >
                                 {job.company}
                             </h5>
-                            <div className="flex flex-row items-center">
-                                <span className="material-symbols-outlined text-red-500">
-                                    location_on
-                                </span>
-                                <span className="text-xs">
-                                    {job.location}
-                                </span>
+                            <div className="flex flex-row justify-between items-center">
+                                <div className="flex flex-row items-center">
+                                    <span className="material-symbols-outlined text-red-500">
+                                        location_on
+                                    </span>
+                                    <span className="text-xs">
+                                        {job.location}
+                                    </span>
+                                </div>
+
+                                <div>
+                                    <span className=
+                                        {
+                                            job.isFavorite ? "material-symbols-outlined filled-icon" : "material-symbols-outlined unfilled-icon"
+                                        }
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            handleFavoriteClick(job.id)
+                                        }}
+                                    >
+                                        favorite
+                                    </span>
+                                </div>
                             </div>
                         </div>
                     ))
                 }
             </div>
-        </div>
+        </div >
     )
 }
 
--- src/services/jobServices.js
+++ src/services/jobServices.js
@@ -6,6 +6,9 @@ const jobServices = {
     },
     getJobByID: async (id) => {
         return await instance.get(`/jobs/${id}`);
+    },
+    updateJob: async (id, job) => {
+        return await instance.put(`/jobs/${id}`, job);
     }
 }
 
