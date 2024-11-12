c91774a84d65ef841a0c26ee01e47d5073b0086d
githubscientist <sathish.datascientist@gmail.com>
2024-11-12 21:27:49 +0530

    db.json -- directly using files + apis

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -4,7 +4,10 @@ const jobRouter = express.Router();
 
 // define routes for the job resource
 jobRouter.get('/', jobController.getJobs);
+jobRouter.get('/:id', jobController.getJob);
 jobRouter.post('/', jobController.createJob);
+jobRouter.put('/:id', jobController.updateJob);
+jobRouter.delete('/:id', jobController.deleteJob);
 
 // export the router
 module.exports = jobRouter;
